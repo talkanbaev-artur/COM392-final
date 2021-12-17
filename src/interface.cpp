5dcf599972144f78e1b47dcdf3659d345f12569c
Talkanbaev Artur <talkanbaev_a@auca.kg>
2021-12-17 15:55:41 +0600

    Added simple yaml config

--- src/interface.cpp
+++ src/interface.cpp
@@ -2,6 +2,8 @@
 #include <cstdlib>
 #include <string.h>
 #include <time.h>
+#include <yaml-cpp/yaml.h>
+#include <iostream>
 
 #include "interface.h"
 #include "gpuCode.h"
@@ -10,7 +12,12 @@
 #include "animate.h"
 #include "crack.h"
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
+	YAML::Node config = YAML::LoadFile("./config/config.yml");
+
+	const std::string version = config["version"].as<std::string>();
+	std::cout << "version: " << version << "\n";
 
 	unsigned char ch;
 	AParams PARAMS;
@@ -18,38 +25,48 @@ int main(int argc, char *argv[]){
 	struct timespec start, finish;
 	double elapsed;
 
-  setDefaults(&PARAMS);
+	setDefaults(&PARAMS);
 
-  // -- update default parameters entered from command line:
-//  if(argc<2){usage(); return 1;} // must be at least one arg (fileName)
-	while((ch = crack(argc, argv, "r|v|f|", 0)) != NULL) {
-	  switch(ch){
-    	case 'r' : PARAMS.runMode = atoi(arg_option); break;
-      case 'v' : PARAMS.verbosity = atoi(arg_option); break;
-			case 'f' : strcpy(PARAMS.fileName, arg_option); break;
-      default : return(0);
-    	}
-  	}
+	// -- update default parameters entered from command line:
+	//  if(argc<2){usage(); return 1;} // must be at least one arg (fileName)
+	while ((ch = crack(argc, argv, "r|v|f|", 0)) != NULL)
+	{
+		switch (ch)
+		{
+		case 'r':
+			PARAMS.runMode = atoi(arg_option);
+			break;
+		case 'v':
+			PARAMS.verbosity = atoi(arg_option);
+			break;
+		case 'f':
+			strcpy(PARAMS.fileName, arg_option);
+			break;
+		default:
+			return (0);
+		}
+	}
 
 	printf("\n -- running virus simulator --\n");
 	runVS(&PARAMS);
 	return 0;
 }
 
-int setDefaults(AParams *PARAMS){
-		// for runmode = 1 (play with pixels);
-		// this overwritten upon opening image file in runmode 1, but runmode 2
-		// assumes a population of 1024 x 1024 = 1048576
-    PARAMS->height     = 1024;
-    PARAMS->width      = 1024;
-    PARAMS->size      = 1024*1024*3; // 800x800 pixels x 3 colorsS
+int setDefaults(AParams *PARAMS)
+{
+	// for runmode = 1 (play with pixels);
+	// this overwritten upon opening image file in runmode 1, but runmode 2
+	// assumes a population of 1024 x 1024 = 1048576
+	PARAMS->height = 1024;
+	PARAMS->width = 1024;
+	PARAMS->size = 1024 * 1024 * 3; // 800x800 pixels x 3 colorsS
 
-		// for runmode = 2 (virus spread simulator)
-	PARAMS->sizePopulation = 1024*1024; // = 1,048,576
-	PARAMS->deadliness = 0.5; // 0 to 1;
-	PARAMS->duration = 7; // number of days a person is infectious before symptoms
-	PARAMS->spreadrate = 0.5;	// 0 to 1
-		// ADD MORE PARAMETERS HERE AND IN PARAMS.H AS NEEDED, ADD TO INTERFACE
+	// for runmode = 2 (virus spread simulator)
+	PARAMS->sizePopulation = 1024 * 1024; // = 1,048,576
+	PARAMS->deadliness = 0.5;			  // 0 to 1;
+	PARAMS->duration = 7;				  // number of days a person is infectious before symptoms
+	PARAMS->spreadrate = 0.5;			  // 0 to 1
+										  // ADD MORE PARAMETERS HERE AND IN PARAMS.H AS NEEDED, ADD TO INTERFACE
 
-    return 0;
+	return 0;
 }
