e3f6f0b63db3279aca326709d0a4295890b6e740
Talkanbaev Artur <talkanbaev_a@auca.kg>
2021-12-18 13:42:48 +0600

    Draft individual apu and update related docs
    Added only basic stats, no functions yet.

---
+++ src/alogrithm/individual.h
@@ -0,0 +1,59 @@
+#ifndef INDIVIDUAL_API_H
+#define INDIVIDUAL_API_H
+
+//Individual is class which stores all stats and state data about
+//a single individual.
+class Individual
+{
+private:
+	// *** MAJOR STATS ***
+
+	//Health status is used to determine the status of human
+	//-1 dead, 0 healthy, 1 infected, 2 ill, 3 recorvering, 4 immune, 5 vaccinated
+	int status;
+	//State is used to determine the exact detail of the current stage
+	//If not dead or health, this number is decremented each day and when reaching 0, changes status
+	int state;
+
+	// *** CHARACTERISTICS ***
+
+	//Susceptibility shows individual health characteristics as one value.
+	//Initialised using the flat normal distribution with low mean value.
+	//THIS IS A VERY GENERAL PARAMETR
+	float susceptibility;
+
+	//Age is self explanatory. Yet to be decided how to generate it logicaly.
+	int age;
+
+	//Vaccination heistancy is used to regulate the rate of vaccination
+	float vaccination_h;
+
+	// *** IMMUNITY ***
+
+	//Immunity is a float value between 0.0 and 1.0, which shows the quality of ones immunity.
+	//It is used to people with statuses 4 and 5, when trying to re-infect them.
+	//
+	//With virus mutating, this value will decay to 0. If it decays lower than 0, the status changes to healthy.
+	float immunity;
+	//Quality of immunity is used to variate the speed of immunity decay
+	float immunity_q;
+
+	//later it would nice to have an n-char descriptor for the virus variation
+	//and assign the immunity based on this dna(rna) sample
+
+public:
+	//Public method to get status. Mainly used to display the population map.
+	getStatus();
+	Individual(/* args */);
+	~Individual();
+};
+
+Individual::Individual(/* args */)
+{
+}
+
+Individual::~Individual()
+{
+}
+
+#endif
