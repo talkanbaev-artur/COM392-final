c2595a298799e8cb17df7c48c59c85bf394835dc
Talkanbaev Artur <talkanbaev_a@auca.kg>
2021-12-19 16:41:07 +0600

    Added draft version of kernel calls structure

---
+++ src/alogrithm/algorithm.cu
@@ -0,0 +1,9 @@
+#include "algorithm.h"
+
+void runDay(SimulationData sd, int day) {}
+
+__global__ void runAlgorithms(SimulationData sd) {}
+
+__device__ void update_statuses() {}
+
+__device__ void infect() {}
