172761e4ccb0d72bbd62c63252f8a99212b1d1ea
Talkanbaev Artur <talkanbaev_a@auca.kg>
2021-12-20 19:22:40 +0600

    Fixed algorithm state bug

--- src/algorithm/algorithm.cu
+++ src/algorithm/algorithm.cu
@@ -7,40 +7,55 @@ void runDay(SimulationData sd, int day) {}
 
 __global__ void runAlgorithms(SimulationData sd) {}
 
-__device__ void update_statuses(Individual *population, Virus *virus, curandState *rand) {
+__device__ void update_statuses(Individual *population, Virus *virus, curandState *rand)
+{
 	int x = threadIdx.x + (blockIdx.x * blockDim.x);
 	int y = threadIdx.y + (blockIdx.y * blockDim.y);
 	int tid = x + (y * blockDim.x * gridDim.x);
 
 	Individual individual = population[tid];
-	
-	switch (individual.status) {
-		case -1: // dead
-			break;
-		case 0: // healthy
-			break;
-		case 1: // infected
-			individual.state -= 1;
-			if (individual.state == 0) individual.status++;
-			individual.state = tnormal(rand, virus->illness_period);
-			break;
-		case 2: // ill
-			individual.state -= 1;
-			if (individual.state == 0) individual.status++;
-			individual.state = tnormal(rand, virus->recovery_period);
-			break;
-		case 3: // recovering
-			individual.state -= 1;
-			if (individual.state == 0) individual.status++;
+	curandState lcu = rand[tid];
+	switch (individual.status)
+	{
+	case -1: // dead
+		break;
+	case 0: // healthy
+		break;
+	case 1: // infected
+		individual.state -= 1;
+		if (individual.state == 0)
+		{
+			individual.status++;
+			individual.state = tnormal(&lcu, virus->illness_period);
+		}
+		break;
+	case 2: // ill
+		individual.state -= 1;
+		if (individual.state == 0)
+		{
+			individual.status++;
+			individual.state = tnormal(&lcu, virus->recovery_period);
+		}
+		break;
+	case 3: // recovering
+		individual.state -= 1;
+		if (individual.state == 0)
+		{
+			individual.status++;
 			individual.state = 100;
-			break;
-		case 4: // immune
-			individual.state -= 1;
-			if (individual.state == 0) individual.status = 0;
-			break;
-		default: // super-human
-			break;
+		}
+		break;
+	case 4: // immune
+	case 5:
+		individual.state -= 1;
+		if (individual.state == 0)
+			individual.status = 0;
+		break;
+	default: // super-human
+		break;
 	}
 
+	rand[tid] = lcu;
 }
+
 __device__ void infect() {}
